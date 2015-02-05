21f775b6fe8808d4843c0629234ff521e1f2b54e
Thomas Castiglione <castiglione@mac.com>
2015-02-05 21:25:07 +0800

    controllers, with views

--- src/metaclicker/Startup.cs
+++ src/metaclicker/Startup.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Diagnostics;
 
 namespace metaclicker
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.UseWelcomePage();
+            app.UseMvc();
+            app.UseErrorPage(ErrorPageOptions.ShowAll);
         }
     }
 }
