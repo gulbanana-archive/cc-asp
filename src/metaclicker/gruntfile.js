db85077afc8f0e481a72f9be84c8b72be2771ca2
Thomas Castiglione <castiglione@mac.com>
2015-02-07 15:45:15 +0800

    add bower

--- src/metaclicker/gruntfile.js
+++ src/metaclicker/gruntfile.js
@@ -1,10 +1,22 @@
-/// <binding AfterBuild='typescript' />
+/// <binding />
 /*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 module.exports = function (grunt) {
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-typescript");
+
     grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    cleanTargetDir: true
+                }
+            }
+        },
         typescript: {
             base: {
                 src: ['Scripts/**/*.ts'],
@@ -17,6 +29,5 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks("grunt-typescript");
-    grunt.registerTask('default', ['typescript']);
+    grunt.registerTask('default', ['bower:install', 'typescript']);
 };
