ce41d6bd4fdb3fc28814c26447f58d52c9775f7c
Thomas Castiglione <castiglione@mac.com>
2015-02-09 02:25:38 +0800

    include some basic react components defined in typescript

--- src/metaclicker/Scripts/main.ts
+++ src/metaclicker/Scripts/main.ts
@@ -1,7 +1,21 @@
 ﻿/// <reference path="Definitions/react-0.13.0.d.ts"/>
 import React = require('react');
 
+class TextBox extends React.Component<{ text: string }, any, any> {
+	render(): React.ReactElement<any> {
+		return React.DOM.div({}, this.props.text);
+	}
+}
+
+class BoldList extends React.Component<{ children: any }, any, any> {
+	render(): React.ReactElement<any> {
+		var boldChildren: any = React.Children.map(this.props.children,(child: React.ReactChild) => { return React.DOM.b({}, child); });
+		return React.DOM.div({}, boldChildren);
+	}
+}
+
 React.render(
-	React.createElement('h1', {}, 'Hello, world!'),
+	React.createElement(BoldList, {}, React.createElement(TextBox, { text: 'a!' }),
+	                                 React.createElement(TextBox, { text: 'b!' })),
 	document.getElementById('content')
 );
