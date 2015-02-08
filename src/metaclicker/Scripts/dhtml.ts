b37d99de725eae3078671e0797760ec6358f77e7
Thomas Castiglione <castiglione@mac.com>
2015-02-08 21:42:45 +0800

    get typescript using react, theoretically

--- src/metaclicker/Scripts/dhtml.ts
+++ src/metaclicker/Scripts/dhtml.ts
@@ -1,1 +1,7 @@
-﻿document.writeln("<p>the deep web</p>");
+﻿/// <reference path="Definitions/react-0.13.0.d.ts"/>
+import React = require('react');
+
+React.render(
+	React.DOM.h1('Hello, world!'),
+	document.getElementById('content')
+);
