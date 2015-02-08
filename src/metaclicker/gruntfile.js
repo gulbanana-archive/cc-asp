c9aa91f43cf03113b96458a97cc75268c5eb6ffc
Thomas Castiglione <castiglione@mac.com>
2015-02-08 16:23:10 +0800

    purge more bower

--- src/metaclicker/gruntfile.js
+++ src/metaclicker/gruntfile.js
@@ -4,19 +4,9 @@ This file in the main entry point for defining grunt tasks and using grunt plugi
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 module.exports = function (grunt) {
-    grunt.loadNpmTasks("grunt-bower-task");
     grunt.loadNpmTasks("grunt-typescript");
 
     grunt.initConfig({
-        bower: {
-            install: {
-                options: {
-                    targetDir: "wwwroot/lib",
-                    layout: "byComponent",
-                    cleanTargetDir: true
-                }
-            }
-        },
         typescript: {
             base: {
                 src: ['Scripts/**/*.ts'],
@@ -29,5 +19,5 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['bower:install', 'typescript']);
+    grunt.registerTask('default', ['typescript']);
 };
