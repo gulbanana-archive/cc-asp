568a8a5af82d2bdd19a524c9bb84e846ff3a6926
Thomas Castiglione <castiglione@mac.com>
2015-02-08 23:28:27 +0800

    theoretically move ts into the browserify pipeline

--- src/metaclicker/gruntfile.js
+++ src/metaclicker/gruntfile.js
@@ -4,33 +4,20 @@ This file in the main entry point for defining grunt tasks and using grunt plugi
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 module.exports = function (grunt) {
-	grunt.loadNpmTasks("grunt-typescript");
 	grunt.loadNpmTasks("grunt-browserify");
 
     grunt.initConfig({
-        typescript: {
-        	dist: {
-            	src: ['Scripts/**/*.ts'],
-				dest: 'wwwroot/lib/',
-                options: {
-                    module: 'commonjs',
-                    target: 'es5',
-                    sourceMap: true,
-					basePath: 'Scripts'
-                }
-            }
-        },
         browserify: {
         	dist: {
-        		src: ['wwwroot/lib/*.js'],
-        		exclude: ['wwwroot/app.js'],
+        		src: ['Scripts/**/*.ts'],
         		dest: 'wwwroot/app.js',
         		browserifyOptions: {
-    				debug: true
-				}
+        			plugin: [['tsify', { noImplicitAny: true }]],
+        			debug: true
+        		},
         	}
         }
     });
 
-    grunt.registerTask('default', ['typescript:dist', 'browserify:dist']);
+    grunt.registerTask('default', ['browserify:dist']);
 };
