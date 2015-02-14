6336e2b6484838133f1aca063ce19c6ef913cec0
Thomas Castiglione <castiglione@mac.com>
2015-02-15 00:24:18 +0800

    do some dynamic state stuff

--- src/metaclicker/Scripts/BoldList.ts
+++ src/metaclicker/Scripts/BoldList.ts
@@ -2,12 +2,25 @@
 import React = require('react');
 import TextBox = require('./TextBox');
 
-class BoldList extends React.Component<{ strings: string[] }, any, any> {
+class BoldList extends React.Component<{ next: () => string }, { strings: string[] }, any> {
+	state = {
+		strings: new Array<string>()
+	}
+
+	componentDidMount() {
+		setTimeout(() => this.pushText(), 100);
+	}
+
 	render() {
-		var textboxes = this.props.strings.map(s => React.createElement(TextBox, { text: s, bold: true }));
+		var textboxes = this.state.strings.map(s => React.createElement(TextBox, { text: s, bold: true }));
 		return React.DOM.div(null, textboxes);
+	}
+
+	pushText() {
+		this.setState({ strings: this.state.strings.concat(this.props.next()) });
+		setTimeout(() => this.pushText(), 100);
 	}
 }
 
-var contract = <React.ComponentClass<{ strings: string[] }, any, any>>BoldList;
+var contract = <React.ComponentClass<{ next: () => string }, { strings: string[] }, any>>BoldList;
 export = contract;
--- src/metaclicker/Scripts/main.ts
+++ src/metaclicker/Scripts/main.ts
@@ -2,7 +2,9 @@
 import React = require('react');
 import BoldList = require('./BoldList');
 
+var x = 0;
+
 React.render(
-	React.createElement(BoldList, { strings: ['a!', 'b!'] }),
+	React.createElement(BoldList, { next: () => { x++; return ""+x; } }),
 	document.getElementById('content')
 );
