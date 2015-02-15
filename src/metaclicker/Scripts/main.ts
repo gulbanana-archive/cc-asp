97c47ab77d04993c91f10af0504320acd9bd5afa
Thomas Castiglione <castiglione@mac.com>
2015-02-15 23:52:31 +0800

    set up a main loop

--- src/metaclicker/Scripts/main.ts
+++ src/metaclicker/Scripts/main.ts
@@ -2,6 +2,8 @@
 import React = require('react');
 import Scene = require('./Scene');
 
+var fps = 5;
+
 var noughts = ['X| | ',
 	           ' |X| ',
 	           ' | |O']
@@ -12,11 +14,47 @@ var dude = [' ( ) ',
 	        ' / \\ ',
 	        '|   |']
 
-React.render(
-	React.createElement(Scene, {
-		entities: [{ name: 'Developer', art: dude },
-			       { name: 'Game', art: noughts },
-			       { name: 'Developer', art: dude }]
-	}),
-	document.getElementById('content')
-);
+function createPlayer() {
+	return { name: 'Player', art: dude }
+}
+
+var state = {
+	entities: [{ name: 'Game', art: noughts }]
+}
+
+function update() {
+	if (state.entities.length < 3) {
+		state.entities.push(createPlayer());
+	} else {
+		var x = state.entities.shift()
+		state.entities.push(x);
+	}
+}
+
+function draw() {
+	React.render(
+		React.createElement(Scene, state),
+		document.getElementById('content')
+	);
+}
+
+var loops = 0;
+var skipTicks = 1000 / fps;
+var maxFrameSkip = 10;
+var nextGameTick = (new Date).getTime();
+
+function run() {
+	loops = 0;
+
+	while ((new Date).getTime() > nextGameTick && loops < maxFrameSkip) {
+		update();
+		nextGameTick += skipTicks;
+		loops++;
+	}
+
+	draw();
+}
+
+var intervalID = setInterval(run, 1000/fps);
+
+
